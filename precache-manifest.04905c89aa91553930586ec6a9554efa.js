b024183c1132982affb83593bf6c041db0d706c5
FrantsevArtem <fran_lg@ukr.net>
2020-10-04 13:42:52 +0300

    Updates

---
+++ precache-manifest.04905c89aa91553930586ec6a9554efa.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b583d1614bb13d655dccccbd7b14bf8d",
+    "url": "/Bootcamp/index.html"
+  },
+  {
+    "revision": "4b8a4a2324a2af4782db",
+    "url": "/Bootcamp/static/css/2.e0686c58.chunk.css"
+  },
+  {
+    "revision": "53079d2b78fa95c5fb6f",
+    "url": "/Bootcamp/static/css/main.a665a322.chunk.css"
+  },
+  {
+    "revision": "4b8a4a2324a2af4782db",
+    "url": "/Bootcamp/static/js/2.a2fd8c84.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Bootcamp/static/js/2.a2fd8c84.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "53079d2b78fa95c5fb6f",
+    "url": "/Bootcamp/static/js/main.6f5cfdff.chunk.js"
+  },
+  {
+    "revision": "ba755de6c4dd8d80fd0f",
+    "url": "/Bootcamp/static/js/runtime-main.b20c623a.js"
+  },
+  {
+    "revision": "0662d9de8af86fb66060a046fbafe1b5",
+    "url": "/Bootcamp/static/media/GC_logo.0662d9de.svg"
+  },
+  {
+    "revision": "2f311a05a79fb86cee6148d658aa8336",
+    "url": "/Bootcamp/static/media/ITN_logo.2f311a05.svg"
+  },
+  {
+    "revision": "43f9dac62ffb9209e823b91b7504c5ec",
+    "url": "/Bootcamp/static/media/energetics.43f9dac6.jpg"
+  },
+  {
+    "revision": "fb084e1e416e340515649cb48eb04f6e",
+    "url": "/Bootcamp/static/media/energy.fb084e1e.jpg"
+  },
+  {
+    "revision": "5d86912d3747f4d2fb2419bd7d2de74b",
+    "url": "/Bootcamp/static/media/hero-bg.5d86912d.jpg"
+  },
+  {
+    "revision": "5ec44869469e2f3c9db3551c44662f88",
+    "url": "/Bootcamp/static/media/industry.5ec44869.jpg"
+  },
+  {
+    "revision": "341a487c2bb1cbed9179faa73d89b343",
+    "url": "/Bootcamp/static/media/shid_logo.341a487c.svg"
+  },
+  {
+    "revision": "f8f67844654cd9fc78dd57834ecfed6c",
+    "url": "/Bootcamp/static/media/sity_planing.f8f67844.jpg"
+  },
+  {
+    "revision": "7e1db0da3d25263be6534e000b29d2a8",
+    "url": "/Bootcamp/static/media/slide-1.7e1db0da.jpg"
+  },
+  {
+    "revision": "12a25e248f105a327021b69336c5b71b",
+    "url": "/Bootcamp/static/media/slide-2.12a25e24.jpg"
+  },
+  {
+    "revision": "638ec2275a294137e028e6bce58ad768",
+    "url": "/Bootcamp/static/media/slide-3.638ec227.jpg"
+  },
+  {
+    "revision": "8a21e8b7fc0908ab482cc0bb0c98138a",
+    "url": "/Bootcamp/static/media/slide-4.8a21e8b7.jpg"
+  },
+  {
+    "revision": "bb9f49bba9c380e18e77de4c1ed82bf6",
+    "url": "/Bootcamp/static/media/slide-5.bb9f49bb.jpg"
+  },
+  {
+    "revision": "65643b05446e10934c8778999860e9de",
+    "url": "/Bootcamp/static/media/transport.65643b05.jpg"
+  },
+  {
+    "revision": "a8b6caeec11d336276f5ed05a63fc938",
+    "url": "/Bootcamp/static/media/utilisation.a8b6caee.jpg"
+  }
+]);
